a7e52d7afe6a9feecd160ee1025e8c3a43c07eca
Akshat Chitransh <akshat.chitransh3108@gmail.com>
2026-01-26 20:44:06 +0530

    button variant created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/button"
+import { StartIcon } from "./icons/startIcon"
+
+
+function App() {
+
+
+  return (
+    <>
+     <div> <Button variant="secondary" text="helloji"  size="md" startIcon={<StartIcon/>} /></div>
+ 
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.tsx
@@ -0,0 +1,34 @@
+import type { ReactElement } from "react";
+import { StartIcon } from "../icons/startIcon";
+
+
+  interface Buttonprops{
+    variant:"primary"|"secondary";
+    size:"sm"|"md"|"lg";
+    text?:string;
+    startIcon?:any;
+    endIcon?: ReactElement;
+    onClick?:()=> void;
+}
+const variantchoice = {
+    "primary":"bg-[#fcba03] ",
+    "secondary":"bg-[#fcba03] ",
+}
+const defaultstyles = "rounded-md ";
+const sizes = {
+  sm: "px-2 py-1 text-sm",
+  md: "px-4 py-2 text-base",
+  lg: "px-6 py-3 text-lg",
+};
+
+
+export const Button = (props:Buttonprops)=>{
+    return<button
+  className={`flex  ${variantchoice[props.variant]} ${defaultstyles} ${sizes[props.size]}`
+  }
+>
+ {props.startIcon?<div className="mr-2">{props.startIcon}</div>:null} {props.text}
+</button>
+
+}
+
